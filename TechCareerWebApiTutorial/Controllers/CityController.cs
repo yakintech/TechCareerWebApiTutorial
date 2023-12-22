8fb85ca24d6f7a908b498fe9672ccaead893e11d
Çağatay Yıldız <yildiz.cagatay@hotmail.com>
2023-12-22 20:48:53 +0300

    web api start

---
+++ TechCareerWebApiTutorial/Controllers/CityController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechCareerWebApiTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        [HttpGet]
+        public string[] Get()
+        {
+            string[] cities = new string[] { "Istanbul", "Ankara", "Izmir", "Bursa", "Antalya" };
+            return cities;
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            string[] cities = new string[] { "Istanbul", "Ankara", "Izmir", "Bursa", "Antalya" };
+
+            // Check if the id is valid
+            if (id < 0 || id >= cities.Length)
+            {
+                return "Invalid ID";
+            }
+            else
+            {
+                return cities[id];
+            }
+        }   
+
+
+    }
+}
---
+++ TechCareerWebApiTutorial/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechCareerWebApiTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+      
+    }
+}
