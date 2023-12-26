4f1d5a0a2953e72e95b98aa091216084a332b9d9
Çağatay Yıldız <yildiz.cagatay@hotmail.com>
2023-12-26 21:13:47 +0300

    blogController

---
+++ TechCareerWebApiTutorial/Controllers/BlogController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechCareerWebApiTutorial.Models.ORM;
+
+namespace TechCareerWebApiTutorial.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BlogController : ControllerBase
+    {
+        private readonly TechCareerDbContext _context;
+
+        public BlogController()
+        {
+            _context = new TechCareerDbContext();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            //databasedeki blogları getir
+            var blogs = _context.Blogs.ToList();
+            return Ok(blogs);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            //disaridan gelen id ile blogu bul
+            //find metodu primary keye göre arama yapar
+            //var blog = _context.Blogs.Find(id);
+
+            //firstordefault metodu icerisine yazdığımız sorguya göre arama yapar
+            var blog = _context.Blogs.FirstOrDefault(x => x.Id == id);
+
+            //blog yoksa 404 döndür
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(blog);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Blog blog)
+        {
+            
+            _context.Blogs.Add(blog);
+            _context.SaveChanges();
+
+            return StatusCode(201, blog);
+        }
+
+        //delete
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            //disaridan gelen id ile blogu bul
+            var blog = _context.Blogs.FirstOrDefault(x => x.Id == id);
+
+            //blog yoksa 404 döndür
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            _context.Blogs.Remove(blog);
+            _context.SaveChanges();
+
+            return Ok(blog);
+        }
+                
+    }
+}
